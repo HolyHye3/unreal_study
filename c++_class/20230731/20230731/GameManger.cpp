167d9b839184070564fe7b06964b7ec18e100099
shkim <shkim9783@gmail.com>
2023-07-31 14:10:12 +0900

    20230731 project ver1

---
+++ c++_class/20230731/20230731/GameManger.cpp
@@ -0,0 +1,67 @@
+#include "GameManger.h"
+
+// 게임 매니저 생성자
+CGameManger::CGameManger():
+	mBattleMgr(nullptr) //0
+{
+}
+
+// 게임 매니저 소멸자
+// 메모리가 제거가 될 때 자동으로 호출된다.
+CGameManger::~CGameManger()
+{
+	if (mBattleMgr)
+	{
+		delete mBattleMgr;
+		mBattleMgr = nullptr;
+	}
+}
+
+EMainMenu CGameManger::Menu()
+{
+	system("cls");
+	std::cout << "1. 전투" << std::endl;
+	std::cout << "2. 상점" << std::endl;
+	std::cout << "3. 가방" << std::endl;
+	std::cout << "4. 종료" << std::endl;
+	std::cout << "메뉴를 선택하세요: ";
+
+	int Input;
+
+	std::cin >> Input;
+
+	if (Input <= (int)EMainMenu::None || Input > (int)EMainMenu::Exit)
+		return EMainMenu::None;
+
+	return (EMainMenu)Input;
+}
+
+bool CGameManger::Init()
+{
+	// 전투 관리자 클래스 생성과 초기화
+	mBattleMgr = new CBattleManager;
+
+	if (!mBattleMgr->Init())
+		return false;
+
+	return true;
+}
+
+void CGameManger::Run()
+{
+	while (true)
+	{
+		switch (Menu())
+		{
+		case EMainMenu::Battle:
+			mBattleMgr->Run();
+			break;
+		case EMainMenu::Store:
+			break;
+		case EMainMenu::Inventory:
+			break;
+		case EMainMenu::Exit:
+			return;
+		}
+	}
+}
--- c++_class/20230731/20230731/GameManger.h
+++ c++_class/20230731/20230731/GameManger.h
@@ -1,12 +1,28 @@
 #pragma once
 
 #include "Info.h"
+#include "BattleManager.h"
+
+enum class EMainMenu
+{
+	None,
+	Battle,
+	Store,
+	Inventory,
+	Exit
+};
 
 class CGameManger
 {
 public:
 	CGameManger();
 	~CGameManger();
+
+private:
+	CBattleManager* mBattleMgr;
+
+private:
+	EMainMenu Menu();
 
 public:
 	bool Init();
