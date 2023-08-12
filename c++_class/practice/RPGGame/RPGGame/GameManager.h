d7cc6ec1a1a04910d035b1fabe67a1797bf68fa4
shkim <shkim9783@gmail.com>
2023-08-12 21:43:16 +0900

    20230812 C++ study

---
+++ c++_class/practice/RPGGame/RPGGame/GameManager.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "info.h"
+
+class CGameManager
+{
+// Singleton패턴을 위한 생성자 private로 선언
+private:
+	CGameManager();
+	~CGameManager();
+
+private:
+	// CSingleton 클래스 동적할당
+	static CGameManager* mInst;
+
+public:
+	
+	static CGameManager* GetInst()
+	{
+		// 메모리 주소를 mInst변수에 저장
+		if (nullptr == mInst)
+			mInst = new CGameManager;
+
+		// nullptr이 아니라면 이미 동적할당된 메모리 주소를 가지고 있으므로 
+		// 그냥 그 주소를 리턴만 해준다.
+		return mInst;
+	}
+
+	static void DestroyInst()
+	{
+		if (nullptr != mInst)
+		{
+
+		}
+	}
+
+
+
+
+};
+
