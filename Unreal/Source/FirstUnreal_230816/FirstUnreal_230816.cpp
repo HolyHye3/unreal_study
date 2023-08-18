b1dac537a9b1859a0efc756d6ae83cc44e95f3e7
shkim <shkim9783@gmail.com>
2023-08-18 21:15:23 +0900

    dir clean

--- Unreal/Source/FirstUnreal_230816/FirstUnreal_230816.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FirstUnreal_230816.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstUnreal_230816, "FirstUnreal_230816" );
--- Unreal/Source/FirstUnreal_230816/FirstUnreal_230816GameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "FirstUnreal_230816GameModeBase.h"
-
